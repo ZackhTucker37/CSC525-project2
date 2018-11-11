4589140f3f73c5d181dcda5f96aa6ce33a49eeca
JosiahMcGurty <JoeMcGurty@gmail.com>
2018-11-11 15:02:20 -0600

    Added font/color options, changed menus to support those.

--- project2.cpp
+++ project2.cpp
@@ -1,12 +1,12 @@
 /*==================================================================================================
  PROGRAMMER:				Zackh Tucker // Josiah McGurty
  COURSE:					CSC 525/625
- MODIFIED BY:				Zackh Tucker <tucker117> // Josiah McGurty <mcgurty1337>
+ MODIFIED BY:				Zackh Tucker <tucker117> // Josiah McGurty <McGurty1337>
  ELECTRONIC FILE LOCATIONS:	\\trace\Class\CSC-525-625\001\tucker117\projects\project2\
 							\\trace\Class\CSC-525-625\001\McGurty1337\projects\project2\
  CONTRIBUTION BREAKDOWN:
- LAST MODIFIED DATE:		11.5.2018
- DESCRIPTION:				Lab project 2, using GLUT to create a
+ LAST MODIFIED DATE:		11.11.2018
+ DESCRIPTION:				Lab project 2, using GLUT to create a simple text editor with a few options.
  NOTE:
  FILES:						project2.cpp, (labProject.sln, ...)
  IDE/COMPILER:				MicroSoft Visual Studio 2017
@@ -26,13 +26,11 @@ using namespace std;
 //****************************************************************************
 
 vector <string> s = { "" };
-int lineNumber = 0;
-int lineHeight = 30;
+int lineNumber = 0; int lineHeight = 30;
 int editorWindow; int infoWindow;
+int font = 2; int color = 1;
 
-GLfloat colorChosen[3] = {0, 0, 0};
-
-int font;
+GLfloat colorChosen[3] = {1, 1, 1};
 
 void drawHelpMessage() {
 	string s1 = "Welcome to the best text editor ever!";
@@ -41,54 +39,54 @@ void drawHelpMessage() {
 	string s4 = "You may hide this window at any time by left-clicking in it.";
 	string s5 = "Use the right-click menu in the Editor window to:";
 	string s6 = "-Display this help window";
-	string s7 = "-Set text options (color, font)";
+	string s7 = "-Select text options (Font, Color)";
 	string s8 = "-Save your work to a text file (C:\\TEMP\\typed.txt)";
 	string s9 = "-Exit the program";
-
 	glColor3f(1.0, 1.0, 1.0);
-
 	glRasterPos2f(5, 245);
 	for (int i = 0; i < s1.length(); ++i) {
-		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, int(s1[i]));
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, int(s1[i]));
 	}
 	glRasterPos2f(5, 215);
 	for (int i = 0; i < s2.length(); ++i) {
-		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, int(s2[i]));
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, int(s2[i]));
 	}
 	glRasterPos2f(5, 185);
 	for (int i = 0; i < s3.length(); ++i) {
-		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, int(s3[i]));
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, int(s3[i]));
 	}
 	glRasterPos2f(5, 155);
 	for (int i = 0; i < s4.length(); ++i) {
-		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, int(s4[i]));
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, int(s4[i]));
 	}
 	glRasterPos2f(5, 125);
 	for (int i = 0; i < s5.length(); ++i) {
-		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, int(s5[i]));
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, int(s5[i]));
 	}
 	glRasterPos2f(45, 95);
 	for (int i = 0; i < s6.length(); ++i) {
-		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, int(s6[i]));
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, int(s6[i]));
 	}
 	glRasterPos2f(45, 65);
 	for (int i = 0; i < s7.length(); ++i) {
-		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, int(s7[i]));
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, int(s7[i]));
 	}
 	glRasterPos2f(45, 35);
 	for (int i = 0; i < s8.length(); ++i) {
-		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, int(s8[i]));
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, int(s8[i]));
 	}
 	glRasterPos2f(45, 05);
 	for (int i = 0; i < s9.length(); ++i) {
-		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, int(s9[i]));
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, int(s9[i]));
 	}
 	glEnd();
 }
 
 void typethings() {
 	glPointSize(1);		// change point size to 1
-	glColor3f(1, 1, 1);
+  if (color == 0) {glColor3f(1.0, 0.0, 0.0);} // change drawing color to red
+  if (color == 1) {glColor3f(0.0, 1.0, 0.0);} // change drawing color to green
+  if (color == 2) {glColor3f(0.193, 0.459, 0.778);} // change drawing color to blue
 	if ((s[lineNumber].length() % 45 == 0 && s[lineNumber].length() != 0) || lineNumber == 0) {
 		//if statement characters in line does not = 0; each line is 45 characters, or line number is 0;
 		lineNumber++;
@@ -97,7 +95,9 @@ void typethings() {
 	for (int h = 0; h <= lineNumber; h++) {
 		glRasterPos2i(0, 1000 - (lineHeight * h));
 		for (int i = 0; i < s[h].length(); i++) {
-			glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, s[h][i]);
+			if (font == 0) {glutBitmapCharacter(GLUT_BITMAP_8_BY_13, s[h][i]);}
+			if (font == 1) {glutBitmapCharacter(GLUT_BITMAP_9_BY_15, s[h][i]);}
+			if (font == 2) {glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, s[h][i]);}
 		}
 	}
 }
@@ -137,7 +137,9 @@ void myInfoMouseCallback(int button, int state, int mouseX, int mouseY) {
 
 //***********************************************************************************
 void runMainMenu(int x) {
-	if (x == 9) {
+	if (x == 9) {exit(0);}
+
+	if (x == 1) { //dump this to a text file instead of console
 		cout << "Message: ";
 		for (int h = 0; h <= lineNumber; h++) {
 			for (int i = 0; i < s[h].length(); i++) {
@@ -145,7 +147,6 @@ void runMainMenu(int x) {
 			}
 		}
 		cout << endl;
-		exit(0);
 	}
 
 	if (x == 0) {
@@ -169,21 +170,9 @@ void runFontMenu(int x) {
 //***********************************************************************************
 void runColorMenu(int x) {
 	myEditorDisplayCallback();
-	if (x == 0) { // Red
-		colorChosen[0] = 1.0;
-		colorChosen[1] = 0.0;
-		colorChosen[2] = 0.0;
-	}
-	if (x == 1) { // Green
-		colorChosen[0] = 0.0;
-		colorChosen[1] = 1.0;
-		colorChosen[2] = 0.0;
-	}
-	if (x == 2) { // Blue
-		colorChosen[0] = 0.193;
-		colorChosen[1] = 0.459;
-		colorChosen[2] = 0.778;
-	}
+	if (x == 0) {color = 0;} // Red
+	if (x == 1) {color = 1;} // Green
+	if (x == 2) {color = 2;} // Blue
 }
 
 //***********************************************************************************
@@ -231,9 +220,9 @@ int main(int argc, char ** argv) {
 	glutKeyboardFunc(myTypingFunc);
 
 	int fontMenu = glutCreateMenu(runFontMenu);
-	glutAddMenuEntry("Times New Roman (10 pt)", 0);
-	glutAddMenuEntry("Helvetica (10 pt)", 1);
-	glutAddMenuEntry("Bitmap Font (8x13)", 2);
+	glutAddMenuEntry("Bitmap Font (8 x 13)", 0);
+	glutAddMenuEntry("Bitmap Font (9 x 15)", 1);
+	glutAddMenuEntry("Helvetica (12 point)", 2);
 
 	int colorMenu = glutCreateMenu(runColorMenu);
 	glutAddMenuEntry("Red", 0);
@@ -244,6 +233,7 @@ int main(int argc, char ** argv) {
 	glutAddMenuEntry("Help", 0);
 	glutAddSubMenu("Font Selection", fontMenu);
 	glutAddSubMenu("Color Selection", colorMenu);
+	glutAddMenuEntry("Save to text (C:\\TEMP\\typed.txt)", 1);
 	glutAddMenuEntry("Exit", 9);
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
 
